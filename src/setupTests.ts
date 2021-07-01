564ed4f1b8e6068f7909a748b51d4a81ef53565e
Katia Duarte <katiaipduarte@hotmail.com>
2021-07-01 16:02:56 +0100

    comment line tmp

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import registerFaIcons from './lib/hooks/registerFaIcons';
-import '@testing-library/react/cleanup-after-each';
+// import '@testing-library/react/cleanup-after-each';
 
 registerFaIcons();
